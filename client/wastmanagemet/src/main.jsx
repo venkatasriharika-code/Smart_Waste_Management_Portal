a90e5e75e75e8229fa0f228fac53174cc87fd825
venkatasriharika-code <sriharikaprathipati@gmail.com>
2026-05-16 18:28:10 +0530

    Update main.jsx

--- client/wastmanagemet/src/main.jsx
+++ client/wastmanagemet/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter,Routes,Route, Link } from 'react-router-dom'
-
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import HomePage from './HomePage.jsx'
 import Login from './components/common/login.jsx'
@@ -14,22 +13,19 @@ import GeoTagging from './components/user/geoTagging.jsx'
 import Purchase from './components/user/purchase.jsx'
 import VehicleMapPage from './components/user/VehicleMapPage.jsx'
 
-
- 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/learn" element={<Learn/>}/>
-          <Route path="/quiz" element={<Quiz/>}/> 
-          <Route path="/track/:vehicleId" element={<VehicleMapPage/>}/>
-          <Route path="/live-tracking" element={<LiveTracking/>}/>
-          <Route path="/geo-tagging" element={<GeoTagging/>}/>
-          <Route path="/purchase" element={<Purchase/>}/> 
-
-        </Routes>
-  </BrowserRouter>,
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/learn" element={<Learn/>}/>
+      <Route path="/quiz" element={<Quiz/>}/>
+      <Route path="/track/:vehicleId" element={<VehicleMapPage/>}/>
+      <Route path="/live-tracking" element={<LiveTracking/>}/>
+      <Route path="/geo-tagging" element={<GeoTagging/>}/>
+      <Route path="/purchase" element={<Purchase/>}/>
+    </Routes>
+  </HashRouter>
 )
